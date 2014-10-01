9db6880dfb3b460d7dce87146c3f26c869e6298b
kmeurer <kevin.a.meurer@gmail.com>
2014-09-30 17:03:21 -0700

    used switch statement.

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,9 +1,3 @@
-/* You should implement your request handler function in this file.
- * And hey! This is already getting passed to http.createServer()
- * in basic-server.js. But it won't work as is.
- * You'll have to figure out a way to export this function from
- * this file and include it in basic-server.js so that it actually works.
- * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
 var returnResponse = function(response, data, code) {
     response.writeHead(code, defaultCorsHeaders);
@@ -20,19 +14,6 @@ var postData = function(request, callback) {
   });
 };
 
-var requestTypes = {
-  getSuccessCode: 200,
-  postSuccessCode: 201,
-  headers: defaultCorsHeaders,
-
-  'OPTIONS': "test",
-
-  'GET': "test",
-
-  'POST': "test"
-
-};
-
 exports.handleRequest = function(request, response) {
 
   console.log("Serving request type " + request.method + " for url " + request.url);
@@ -41,45 +22,22 @@ exports.handleRequest = function(request, response) {
   // Declare default headers to handle CORS
   var headers = defaultCorsHeaders;
 
-  // OPTIONS REQUESTS
-  if (request.method === 'OPTIONS') {
+  switch(request.method){
+    case 'OPTIONS':
       response.writeHead(200, headers);
       response.end();
-  }
-  // GET REQUESTS
-
-  if (request.method === 'GET'){
+      break;
+    case 'GET':
       returnResponse(response, JSON.stringify({results: exports.messageStorage}), getSuccessCode);
-    // if (request.url === "/log"){
-    // } else if( request.url === '/classes/room1' ){
-    //   returnResponse(response, JSON.stringify({results: exports.messageStorage}), getSuccessCode);
-    // } else if(request.url === '/classes/messages') {
-    //   returnResponse(response, JSON.stringify({results: exports.messageStorage}), getSuccessCode);
-    // } else {
-    //   returnResponse(response, 'Error 404: Not Found!', 404)
-    // }
-  }
-
-  // POST REQUESTS
-  if (request.method === "POST"){
-    if ( request.url === "/send" ){
+      break;
+    case 'POST':
       postData(request, function(data){
         exports.messageStorage.unshift(JSON.parse(data));
       });
       returnResponse(response, JSON.stringify({results: exports.messageStorage}), postSuccessCode);
-    } else if (request.url === "/classes/messages" ){
-      postData(request, function(data){
-        exports.messageStorage.unshift(JSON.parse(data));
-      });
-      returnResponse(response, JSON.stringify({results: exports.messageStorage}), postSuccessCode);
-    } else if (request.url === "/classes/room1" ){
-      postData(request, function(data){
-        exports.messageStorage.unshift(JSON.parse(data));
-      })
-      returnResponse(response, JSON.stringify({results: exports.messageStorage}), postSuccessCode);
-    } else {
-      returnResponse(response, 'Error 404: Not Found!', 404)
-    }
+      break;
+    default:
+      returnResponse(response, null, 404);
   }
 };
 
